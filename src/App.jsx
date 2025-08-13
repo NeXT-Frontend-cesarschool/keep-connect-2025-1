e4db7bbbf50543048b9802797cd5bcf8d4ed78e9
Djonathas Cardoso <djonathas@gmail.com>
2025-08-13 15:36:21 -0300

    Remove biome and add asset

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
-import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Copyright from "./Copyright";
 import ProTip from "./ProTip";
+import ReactLogo from './assets/react.svg?react';
+import SvgIcon from "@mui/material/SvgIcon";
 
 export default function App() {
   return (
-    <Container>
-      <Box sx={{ my: 4 }}>
+    <Container sx={{ minHeight: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
+      <Stack sx={{ alignItems: "center" }}>
+        <SvgIcon component={ReactLogo} inheritViewBox sx={{ width: 100, height: 100, mb: 2 }} />
         <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
           Material UI Vite.js example
         </Typography>
         <ProTip />
         <Copyright />
-      </Box>
+      </Stack>
     </Container>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import { StrictMode } from "react";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import { createRoot } from "react-dom/client";
@@ -9,11 +10,11 @@ const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <ThemeProvider theme={theme}>
       {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
       <CssBaseline />
       <RouterProvider router={router} />
     </ThemeProvider>
-  </React.StrictMode>
+  </StrictMode>
 );
