f6e9986a93ceb5639b1b66d9df06937401d0b740
Lucas Marques <luandrealbuquerque@gmail.com>
2025-08-14 21:41:02 -0300

    criar/deletar/listar users

--- src/FormPage.jsx
+++ src/FormPage.jsx
@@ -2,13 +2,34 @@ import { Button, Container, Stack, TextField, Typography } from "@mui/material";
 import AppTitle from "./AppTitle";
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { useNavigate } from "react-router";
-
 export default function FormPage() {
-    const navigate = useNavigate()
+  const navigate = useNavigate()
 
-    const goBack = () => {
-        navigate('/')
-    }
+  const goBack = () => {
+    navigate('/')
+  }
+
+  const cadastrarUsuario = (event) => {
+    event.preventDefault()
+
+    const form = new FormData(event.target)
+    const data = Object.fromEntries(form)
+
+    fetch('http://localhost:4000/contacts', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        window.alert('Sucesso!');
+        event.target.reset();
+      })
+      .catch(error => console.log(error))
+  }
 
   return (
     <Container
@@ -27,12 +48,14 @@ export default function FormPage() {
         Novo contato
       </Typography>
 
-      <Stack gap={2} mb={4}>
-        <TextField variant="filled" label="Nome" />
-        <TextField variant="filled" label="E-mail" />
-        <TextField variant="filled" label="Telefone" />
-      </Stack>
-      <Button variant="contained">Cadastrar</Button>
+      <form onSubmit={cadastrarUsuario}>
+        <Stack gap={2} mb={4}>
+          <TextField variant="filled" label="Nome" name="name" />
+          <TextField variant="filled" label="E-mail" name="email" />
+          <TextField variant="filled" label="Telefone" name="phone" />
+        </Stack>
+        <Button variant="contained" type="submit" fullWidth>Cadastrar</Button>
+      </form>
     </Container>
   );
 }
--- src/Home.jsx
+++ src/Home.jsx
@@ -6,16 +6,46 @@ import {
   Divider,
   SvgIcon,
   Box,
+  IconButton
 } from "@mui/material";
 import EmptyBox from "./assets/empty-box.svg?react";
 import { useNavigate } from "react-router";
 import AppTitle from "./AppTitle";
+import { useState, useEffect } from "react";
+import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
 
 export default function Home() {
   const navigate = useNavigate();
 
   const goToForm = () => {
     navigate('form')
+  }
+
+  const [contacts, setContacts] = useState([])
+
+  const getContacts = () => {
+    fetch('http://localhost:4000/contacts')
+      .then((response) => response.json())
+      .then((data) => setContacts(data))
+      .catch((error) => console.log(error))
+  }
+
+  useEffect(() => {
+    getContacts()
+  }, [])
+
+  const deleteUser = (id, name) => {
+    const confirm = window.confirm(`Queres deletar mesmo ${name}?`)
+    if (!confirm) return;
+
+    fetch(`http://localhost:4000/contacts/${id}`, { method: 'DELETE' })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        getContacts();
+      })
+      .catch(erro => console.log(erro))
   }
 
   return (
@@ -34,20 +64,50 @@ export default function Home() {
       </Button>
       <Divider />
 
-      <Stack alignItems="center" gap={2} mt={4}>
-        <SvgIcon
-          component={EmptyBox}
-          inheritViewBox
-          sx={{ width: 110, height: 77 }}
-        />
-        <Typography width={400} fontWeight="bold" color="#BCBCBC" textAlign="center">
-          Você ainda não tem nenhum contato cadastrado! Clique no botão{" "}
-          <Box component="span" color="primary.dark">
-            “Novo contato”
-          </Box>{" "}
-          acima para cadastrar o seu primeiro!
-        </Typography>
-      </Stack>
+      {
+        contacts.length === 0 && (
+          <Stack alignItems="center" gap={2} mt={4}>
+            <SvgIcon
+              component={EmptyBox}
+              inheritViewBox
+              sx={{ width: 110, height: 77 }}
+            />
+            <Typography width={400} fontWeight="bold" color="#BCBCBC" textAlign="center">
+              Você ainda não tem nenhum contato cadastrado! Clique no botão{" "}
+              <Box component="span" color="primary.dark">
+                “Novo contato”
+              </Box>{" "}
+              acima para cadastrar o seu primeiro!
+            </Typography>
+          </Stack>
+        )
+      }
+
+      <div style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
+        {
+          contacts.map((contact) => (
+            <div
+              key={contact.id}
+              style={{
+                padding: 16,
+                borderRadius: '8px',
+                boxShadow: '0px 0px 3px 0px rgba(0, 0, 0, 0.2)',
+                display: 'flex',
+                justifyContent: 'space-between'
+              }}>
+              <div style={{ display: 'flex', flexDirection: 'column', gap: '4px' }}>
+                <span style={{ fontWeight: 'bold' }}>{contact.name}</span>
+                <span style={{ color: '#BCBCBC', fontSize: '14px' }}>{contact.email}</span>
+                <span style={{ color: '#BCBCBC', fontSize: '14px' }}>{contact.phone}</span>
+              </div>
+              <div style={{ alignContent: 'center' }}>
+                <IconButton color="primary"><EditOutlinedIcon /></IconButton>
+                <IconButton color="primary" onClick={() => deleteUser(contact.id, contact.name)}><DeleteOutlineOutlinedIcon /></IconButton>
+              </div>
+            </div>
+          ))
+        }
+      </div>
     </Container>
   );
 }
